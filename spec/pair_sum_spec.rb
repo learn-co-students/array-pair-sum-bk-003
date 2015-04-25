79f232f49aa09ce03ca0451a183f4512be0a88c2
Katie Hoffman <ktahoffman@gmail.com>
2015-04-25 15:39:22 -0400

    add sixth test

--- spec/pair_sum_spec.rb
+++ spec/pair_sum_spec.rb
@@ -1,7 +1,6 @@
 describe Array do
   describe "#pair_sum" do
-
-    it "doesn't change the original array" do
+    it"doesn'tchange the original array" do
       numbers = [2,3,5,1,4,7,0,6]
       numbers.pair_sum(5)
       expect(numbers).to eq(numbers)
@@ -25,6 +24,11 @@ describe Array do
     it "returns four pairs that add up to eight" do
       numbers = [8,1,0,4,6,10,2,4,-2]
       expect(numbers.pair_sum(8)).to eq([[-2,10],[0,8],[2,6],[4,4]])
+    end
+
+    it "returns five paris that add up to thirty-one" do
+      numbers = [-28,30,-50,39,39,25,35,24,-11,1,23,-37,36,-46,1,31,39,-14,-23,-18,-27,-25,48,25,39,-47,-23,-3,9,-27,-45,49,-30,-41,-7,8,-50,-17,-20,47,-4,-14,15,-22,-2,-14,-42,-31,-36,28]
+      expect(numbers.pair_sum(31)).to eq([[-18, 49],[-17,48],[-4,35],[1,30],[8, 23]])
     end
   end
 end
