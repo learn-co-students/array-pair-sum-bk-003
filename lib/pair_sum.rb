2ac424a670fc7096ef69dab267b93315e53675b4
Umer <myemailum14@gmail.com>
2015-11-15 18:18:29 -0500

    done

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,13 @@
 # your code goes here
+class Array
+	def pair_sum(t)
+		matches = []
+		self.each.with_index { |n, i|
+			break if (i == self.size - 1 )
+			self[(i+1)..-1].each { |e|
+				matches << [n,e].sort if (e+n == t)
+			}
+		}
+		matches.sort.uniq
+	end 
+end
