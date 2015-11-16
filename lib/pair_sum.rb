539f1ba3c2e5450ea262bd0ebc44187045e573eb
dylan.okeefe <dylanokeefe@gmail.com>
2015-11-16 10:35:15 -0500

    paired

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,18 @@
-# your code goes here
+class Array
+
+  def pair_sum(sum)
+    @final_array = []
+    @sum_array = self
+    self.each_with_index do |number, index|
+      @sum_array.each_with_index do |sum_number, sum_index|
+      @inside_array = []
+        if (number + sum_number == sum) && index != sum_index
+          @inside_array.push(self[index], @sum_array[sum_index])
+          @final_array.push(@inside_array.sort)
+        end
+      end
+    end
+    @final_array.uniq!.sort
+  end
+  
+end
