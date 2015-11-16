91734e2ae8e15e77cfaa22f1799a460777834936
Lawrence Chong <mail.to.lawrence@gmail.com>
2015-11-16 10:06:13 -0500

    fin

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Array
+
+	def pair_sum(num)
+		array = self.sort
+		return_array = []
+		array.each_with_index { |e,index|  
+			a = num - e
+			break if a < e
+			a_index = array.index(a)
+			return_array << [e,a] if !a_index.nil? and a_index != index
+		}
+		return_array.uniq!
+		return return_array
+	end
+
+end
