75b862056198658b8f60317d0f69e4daf661ee9b
cloudleo <l.castillo1989@hotmail.com>
2015-11-29 21:41:37 -0500

    you better call tyrone

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require 'pry'
+class Array
+
+
+def pair_sum(num)
+  array = []
+  self.sort.each do |m| 
+   self.sort.reverse.each do |i|
+    if (m <= num)
+      ar = []
+       ar << m
+       if (i + ar[0] == num)
+        ar << i
+        array << ar.sort
+      
+      end
+  end
+
+end
+end
+return array.uniq
+end
+
+end
