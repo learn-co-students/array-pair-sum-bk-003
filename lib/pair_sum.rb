1c40b3ea57d311772985d9ebc2f5f218baaf7c0c
github username <mendelblesofsky@gmail.com>
2015-12-03 16:26:34 -0500

    pierre paired with a peer and peered at pureed pears

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,8 @@
 # your code goes here
+class Array
+	def pair_sum(n)
+		answer = []
+		self.each { |num| self.length.times { |i| answer << [num, self[i]].sort if num + self[i] == n } }
+		answer.uniq.sort
+	end
+end
