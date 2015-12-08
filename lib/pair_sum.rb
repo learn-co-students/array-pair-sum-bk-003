f2d769a8f8ab6822bf41b992c43d36fb17fd4cab
Peter Han <han.peter92@gmail.com>
2015-12-08 11:11:16 -0500

    done

--- lib/pair_sum.rb
+++ lib/pair_sum.rb
@@ -1,1 +1,17 @@
 # your code goes here
+class Array
+	def pair_sum(num)
+		new_array = self.clone
+		container = []
+		new_array.sort!
+		new_array.each_with_index do |e,i|
+			while i + 1 < new_array.length
+				if e + new_array[i+1] == num
+					container << [e, new_array[i+1]]
+				end
+				i += 1
+			end
+		end
+		container.uniq
+	end
+end
